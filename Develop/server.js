01f63e5d0213c51087665b384668082ced893347
Walsh Vaz <walshivaz@yahoo.com>
2023-04-25 17:22:49 -0400

    added server.js

---
+++ Develop/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const readFromFile = util.promisify(fs.readFile);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/notes.html")));
+
+app.get("*", (req, res) => 
+res.sendFile(path.join(__dirname, "/public/index.html")));
+
+app.get("/api/notes", (req, res) => {
+    console.info(`${req.method}`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} `);
+
+    const {title, text} = req.body;
+
+    if(req.body){
+        const newNote = {
+            title,
+            text,
+        };
+
+        fs.readFile("./db/db.json", 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+            } 
+            else {
+              const parsedData = JSON.parse(data);
+              parsedData.push(newNote);
+              
+              fs.writeFile("./db/db.json", JSON.stringify(parsedData, null, 4), (err) => 
+              err ? console.error(err) : console.info(`\nData written to ./db/db.json`));
+            }
+          })
+          res.json(`notes added`);
+        }
+    else {
+        res.error("error displayed");
+    }
+});
+
+
+
+app.listen(PORT, () => 
+console.log(`app is working at http://localhost:${PORT}`));
